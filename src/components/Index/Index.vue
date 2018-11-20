5f206dd46eaba94ed8768921c980228bfe15b03d
钟兆立 <525966315@qq.com>
2018-11-20 11:10:49 +0800

    修改

--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -4,19 +4,43 @@
             
         </div>
         <div class="ke_banner">
-            <div class="kx_banner_header">
+            <div class="ke_banner_header">
                 <div class="logo">Keeping</div>
-                <div class="kx_right_menu">
+                <div class="ke_right_menu">
                     <i class="layui-icon layui-icon-shrink-right"></i>  
                 </div>
             </div>
-            <div class="kx_banner_text">
-                <div>运动</div>
+            <div class="ke_banner_text">
+                <div>Just Do It!</div>
             </div>
-            <video autoplay loop="loop" src="../../assets/banner.mp4"></video>
-            <div class="kx_small_banner"></div>
+            <video autoplay loop="loop" src="@/assets/banner.mp4"></video>
+            <div class="ke_small_banner"></div>
         </div>
-        <div class="kx_body">
+        <div class="ke_body">
+            <div class="ke_why_big">
+                <div class="ke_why_div layui-col-md8 layui-col-md-offset2">
+                    <img src="@/assets/images/pic01.jpg">
+                    <div class="ke_why_text_div">
+                        <div class="ke_why_title">
+                            <h1>LOREM IPSUM DOLOR</h1>
+                            <div>maecenas feugiat ex purus, quis volutpat lacus placerat</div>
+                        </div>
+                        <hr>
+                        <div class="ke_why_content">
+                            <div>Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada. Praesent in sem ex. Morbi mattis sapien pretium tellus venenatis, at egestas urna ornare.</div>
+                            <br>
+                            <div>Vivamus fermentum nibh vel pharetra blandit. Cras a purus urna. Sed et libero ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse id hendrerit felis. Nulla viverra tempor dui at congue. Pellentesque quis nulla ornare, congue nisi id, finibus nulla. Aliquam sit amet hendrerit purus. Donec libero massa, posuere fermentum eros sit amet, maximus fringilla augue. Maecenas at rhoncus lorem. Vivamus ultricies consequat est, efficitur convallis libero. Vivamus rutrum semper mauris, vitae consequat eros tempor ac. Pellentesque et ornare sapien</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ke_course_div layui-col-md12">
+                <div class="ke_course_list layui-col-md8 layui-col-md-offset2">
+                    <div class="layui-col-md4">dsadas</div>
+                    <div class="layui-col-md4">dsa</div>
+                    <div class="layui-col-md4">dsadsadas</div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
