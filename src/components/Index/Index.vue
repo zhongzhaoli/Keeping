97428f9572a349f05b2e09e26a543ef4f9cb2da2
钟兆立 <525966315@qq.com>
2018-11-20 15:31:52 +0800

    首页完成

--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -34,11 +34,65 @@
                     </div>
                 </div>
             </div>
-            <div class="ke_course_div layui-col-md12">
-                <div class="ke_course_list layui-col-md8 layui-col-md-offset2">
-                    <div class="layui-col-md4">dsadas</div>
-                    <div class="layui-col-md4">dsa</div>
-                    <div class="layui-col-md4">dsadsadas</div>
+            <div class="ke_course_div">
+                <div class="ke_course_list layui-col-lg8 layui-col-md10 layui-col-sm12 layui-col-xs12 layui-col-lg-offset2 layui-col-md-offset1 layui-col-sm-offset0 layui-col-xs-offset0">
+                    <div class="layui-col-lg4 layui-col-md4 layui-col-sm4 layui-col-xs12">
+                        <h1>课程一</h1>
+                        <div class="course_text">
+                            Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu
+                        </div>
+                        <button class="ke_button">原始按钮</button>
+                    </div>
+                    <div class="layui-col-lg4 layui-col-md4 layui-col-sm4 layui-col-xs12">
+                        <h1>课程二</h1>
+                        <div class="course_text">
+                            Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu
+                        </div>
+                        <button class="ke_button">原始按钮</button>
+                    </div>
+                    <div class="layui-col-lg4 layui-col-md4 layui-col-sm4 layui-col-xs12">
+                        <h1>课程三</h1>
+                        <div class="course_text">
+                            Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu
+                        </div>
+                        <button class="ke_button">原始按钮</button>
+                    </div>
+                </div>
+            </div>
+            <div class="ke_room_big">
+                <div class="ke_room_div layui-col-lg8 layui-col-md10 layui-col-sm12 layui-col-xs12 layui-col-lg-offset2 layui-col-md-offset1 layui-col-sm-offset0 layui-col-sm-offset0">
+                    <div class="layui-col-lg6 layui-col-md6 layui-col-sm12 layui-col-xs12">
+                        <img src="@/assets/images/pic02.jpg" alt="">
+                        <div class="room_text">
+                            <div class="room_title">
+                                <h1>LOREM IPSUM DOLOR</h1>
+                                <div>maecenas feugiat ex purus quis volutpat.</div>
+                            </div>
+                            <hr>
+                            <div class="room_content">
+                                <div>Vivamus fermentum nibh vel pharetra blandit. Cras a purus urna. Sed et libero ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse id hendrerit felis. Nulla viverra tempor dui at congue. Pellentesque quis nulla ornare, congue nisi id, finibus nulla. Aliquam sit amet hendrerit purus. Donec libero massa, posuere fermentum eros sit amet, maximus fringilla augue. Maecenas at rhoncus lorem. Vivamus ultricies consequat est, efficitur convallis libero. Vivamus rutrum semper mauris, vitae consequat eros tempor ac. Pellentesque et ornare sapien</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="layui-col-lg6 layui-col-md6 layui-col-sm12 layui-col-xs12">
+                        <img src="@/assets/images/pic03.jpg" alt="">
+                        <div class="room_text">
+                            <div class="room_title">
+                                <h1>LOREM IPSUM DOLOR</h1>
+                                <div>maecenas feugiat ex purus quis volutpat.</div>
+                            </div>
+                            <hr>
+                            <div class="room_content">
+                                <div>Vivamus fermentum nibh vel pharetra blandit. Cras a purus urna. Sed et libero ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse id hendrerit felis. Nulla viverra tempor dui at congue. Pellentesque quis nulla ornare, congue nisi id, finibus nulla. Aliquam sit amet hendrerit purus. Donec libero massa, posuere fermentum eros sit amet, maximus fringilla augue. Maecenas at rhoncus lorem. Vivamus ultricies consequat est, efficitur convallis libero. Vivamus rutrum semper mauris, vitae consequat eros tempor ac. Pellentesque et ornare sapien</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ke_us">
+                <h1>联系我们</h1>
+                <div class="us_text">
+                    Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu
                 </div>
             </div>
         </div>
