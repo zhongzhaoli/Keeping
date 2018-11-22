8e5cb3ce0b48ee275281f6b99b981c4f003da934
钟兆立 <525966315@qq.com>
2018-11-22 09:20:00 +0800

    字体修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index/Index'
+import Input from '@/components/Input/Input'
 
 Vue.use(Router)
 
@@ -13,6 +14,10 @@ export default new Router({
     {
       path: '/index',
       component: Index
+    },
+    {
+      path: '/input',
+      component: Input
     },
   ]
 })
